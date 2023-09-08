f2399bb19136509a81b97ee3e95af803192fcff7
Phan Thanh Hoang <hoanghkds2@gmail.com>
2023-09-08 09:10:43 +0700

    first commit

---
+++ src/Common/Algorithm.java
@@ -0,0 +1,43 @@
+package Common;
+
+public class Algorithm {
+     public void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // tra ve index cua phan tu neu tim thay
+            }
+        }
+        return -1;
+    }
+}
